de1c6942c205187ff87526ab26df1630c9e94df4
haryarrahman <haryarrahman7@gmail.com>
2023-09-13 23:33:10 +0700

    fix: try to fix

--- server.mjs
+++ server.mjs
@@ -19,6 +19,11 @@ const clients = [];
 let tradingData = [];
 
 app.get("/sse", (req, res) => {
+  if (req.url == "/__webpack_hmr") {
+    res.type("text/event-stream");
+    return;
+  }
+
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
 
